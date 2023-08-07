7d8a17378e21625afd3450b9b9e32e0c45a65fb0
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2023-08-07 15:43:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    member.style.display = "block";
+    skills.style.display = "none";
+
+}
